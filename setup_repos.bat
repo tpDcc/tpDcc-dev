380fa978c666fd79ddf321a44a1970f5883447f8
Tomas Poveda <tpovedatd@gmail.com>
2021-03-26 02:30:09 +0100

    Added bat files to automatically download all tpDcc repos from GitHub

---
+++ setup_repos.bat
@@ -0,0 +1,24 @@
+if not exist tpDcc mkdir tpDcc
+cd tpDcc
+git clone https://github.com/tpDcc/tpDcc-config.git
+git clone https://github.com/tpDcc/tpDcc-core.git
+git clone https://github.com/tpDcc/tpDcc-dccs-houdini.git
+git clone https://github.com/tpDcc/tpDcc-dccs-max.git
+git clone https://github.com/tpDcc/tpDcc-dccs-maya.git
+git clone https://github.com/tpDcc/tpDcc-dccs-mobu.git
+git clone https://github.com/tpDcc/tpDcc-dccs-standalone.git
+git clone https://github.com/tpDcc/tpDcc-dccs-unreal.git
+git clone https://github.com/tpDcc/tpDcc-libs-curves.git
+git clone https://github.com/tpDcc/tpDcc-libs-datalibrary.git
+git clone https://github.com/tpDcc/tpDcc-libs-nameit.git
+git clone https://github.com/tpDcc/tpDcc-libs-options.git
+git clone https://github.com/tpDcc/tpDcc-libs-python.git
+git clone https://github.com/tpDcc/tpDcc-libs-qt.git
+git clone https://github.com/tpDcc/tpDcc-libs-resources.git
+git clone https://github.com/tpDcc/tpDcc-libs-unittests.git
+git clone https://github.com/tpDcc/tpDcc-plugins-maya-helloworld.git
+git clone https://github.com/tpDcc/tpDcc-tools-datalibrary.git
+git clone https://github.com/tpDcc/tpDcc-tools-nameit.git
+git clone https://github.com/tpDcc/tpDcc-tools-renamer.git
+git clone https://github.com/tpDcc/tpDcc-tools-scripteditor.git
+git clone https://github.com/tpDcc/tpDcc-tools-unittests.git
